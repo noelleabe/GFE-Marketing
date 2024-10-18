e43ec71c35d61482ce4f3de67ae55de25727b366
noelleabe <28873286+noelleabe@users.noreply.github.com>
2024-10-17 17:25:28 -0700

    removes duplicate testimonial card

--- src/app/ui/testimonial-card/TestimonialCard.tsx
+++ src/app/ui/testimonial-card/TestimonialCard.tsx
@@ -1,5 +1,4 @@
 import Image, { StaticImageData } from "next/image";
-import Card from "../Card";
 
 interface ITestimonialCardProps {
   imageSrc: StaticImageData;
@@ -17,17 +16,15 @@ export default function TestimonialCard({
   username,
 }: ITestimonialCardProps) {
   return (
-    <Card shadow>
-      <div className="p-6">
-        <div className="flex flex-row">
-          <Image src={imageSrc} height={48} width={48} alt={imageAlt} />
-          <div className="ml-4">
-            <p className="text-lg text-neutral-900 font-semibold">{fullName}</p>
-            <p className="text-sm text-neutral-600">{username}</p>
-          </div>
+    <div className="w-[340px] p-6 m-auto bg-white rounded-lg drop-shadow-3xl">
+      <div className="flex flex-row">
+        <Image src={imageSrc} height={48} width={48} alt={imageAlt} />
+        <div className="ml-4">
+          <p className="text-lg text-neutral-900 font-semibold">{fullName}</p>
+          <p className="text-sm text-neutral-600">{username}</p>
         </div>
-        <div className="mt-4 text-base text-neutral-600">{description}</div>
       </div>
-    </Card>
+      <div className="mt-4 text-base text-neutral-600">{description}</div>
+    </div>
   );
 }
